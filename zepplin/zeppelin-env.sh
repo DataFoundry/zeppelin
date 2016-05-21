9e6e82bb2219f803305e070600473091c5216764
yepengxj <yepeng_xj@163.com>
2016-05-21 21:29:18 +0800

    Update zeppelin-env.sh

--- zepplin/zeppelin-env.sh
+++ zepplin/zeppelin-env.sh
@@ -13,8 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-if [ ${SPARKBSI} ]; then
+if [[ -n "${SPARKBSI}" ]]; then
     eval SPARK_MASTER=\$BSI_${SPARKBSI}_HOST
+    eval SPARK_SECRET=\$BSI_${SPARKBSI}_PASSWORD
 fi
 
 
